e2e8d5ef648542cbe87cfea4dbd2a21f0e5b3b5b
Reasonably Selenium <reasonablyselenium@gmail.com>
2020-05-08 22:33:00 -0400

    adding code to load manifest.json

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue) {
+export default function (Vue, {head}) {
+  head.link.push({
+    rel: 'manifest',
+    href: '/manifest.json'
+  })
   Vue.component('Layout', DefaultLayout)
 }
