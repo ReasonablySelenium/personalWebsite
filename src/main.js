74312c0100562e20189142d1fbee3b4f9a8f0779
Reasonably Selenium <reasonablyselenium@gmail.com>
2020-05-09 01:19:54 -0400

    Giving even less priority to manifest.

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, {head}) {
   Vue.component('Layout', DefaultLayout)
-  head.link.push({
-    rel: 'preload',
-    href: '/manifest.json',
-    as: 'manifest',
-    body: true
-  })
   head.meta.push({
     name: "theme-color"
+  })
+  head.link.push({
+  rel: 'preload',
+  href: '/manifest.json',
+  as: 'manifest'
   })
 }
