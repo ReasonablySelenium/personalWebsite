075bf3d5c6e7202a8a752017ccb8485df3573579
Reasonably Selenium <reasonablyselenium@gmail.com>
2020-05-09 00:50:22 -0400

    making layout first priority

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, {head}) {
+  Vue.component('Layout', DefaultLayout)
   head.link.push({
     rel: 'manifest',
     href: '/manifest.json'
   })
-  Vue.component('Layout', DefaultLayout)
 }
